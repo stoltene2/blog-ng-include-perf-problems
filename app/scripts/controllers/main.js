de19a8ab60e01e56d39d36b4078e80edfd44c3b8
Eric Stolten <stoltene2@gmail.com>
2015-04-14 20:55:02 -0400

    Second commit, rough structure no style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,35 @@
 
 angular.module('app')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.showDetails = false;
+    $scope.toggleDetails = function() {
+      $scope.showDetails = !$scope.showDetails;
+    };
+
+    $scope.products = [ ];
+
+    var product = {
+      name: 'Guide to fast AngularJS apps',
+      price: 10.35,
+      description: 'A potentially really long description',
+      specifications: [
+        {
+          name: 'weight',
+          value: '3'
+        }
+      ],
+      reviews: [
+        {
+          userName: 'Eric Stolten',
+          rating: 5,
+          review: 'Really long review'
+        }
+      ]
+    };
+
+    for(var i = 0; i < 100; i++) {
+      $scope.products.push(product);
+    }
+
   });
