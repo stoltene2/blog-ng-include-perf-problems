f4948986129a314e5da48b68e971edf91dbb7070
Eric Stolten <stoltene2@gmail.com>
2015-04-18 11:10:19 -0400

    Split view into finer grained views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,34 +3,66 @@
 angular.module('app')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.showDetails = false;
-    $scope.toggleDetails = function() {
-      $scope.showDetails = !$scope.showDetails;
+    $scope.expand = function(p) {
+      p.expanded = !p.expanded;
     };
 
     $scope.products = [ ];
 
+    $scope.form = {totalProducts: 10};
+
     var product = {
+      expanded: false,
       name: 'Guide to fast AngularJS apps',
       price: 10.35,
-      description: 'A potentially really long description',
+      description: 'Lorem ipsum dolor sit callet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       specifications: [
         {
           name: 'weight',
           value: '3'
+        },
+        {
+          name: 'length',
+          value: '20'
+        },
+        {
+          name: 'height',
+          value: '10'
+        },
+        {
+          name: 'color',
+          value: '30'
         }
+
       ],
       reviews: [
         {
           userName: 'Eric Stolten',
           rating: 5,
-          review: 'Really long review'
+          review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          userName: 'Eric Stolten',
+          rating: 3,
+          review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          userName: 'Eric Stolten',
+          rating: 1,
+          review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         }
       ]
     };
 
-    for(var i = 0; i < 100; i++) {
-      $scope.products.push(product);
-    }
+    $scope.generateProducts = function() {
+      var ps = [];
 
+      for(var i = 0; i < $scope.form.totalProducts; i++) {
+        ps.push(product);
+      }
+
+      $scope.products = ps;
+    };
+
+    $scope.generateProducts();
   });
